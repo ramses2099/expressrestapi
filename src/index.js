4847cfc39f84f3d56c89d16f9fca31b285d0a4f6
ramses2099 <ramses2099@gmail.com>
2022-08-25 15:01:34 -0400

    add service controller database

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// In src/index.js 
+const express = require("express"); 
+const v1Router = require("./v1/routes");
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+// ROUTERS 
+app.use("/api/v1",v1Router);
+
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
